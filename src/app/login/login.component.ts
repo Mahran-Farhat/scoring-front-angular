1e4b478ca13063160b9e404242921508ac9e3b12
Mahran-Farhat <farhatmahran@gmail.com>
2021-12-10 11:47:53 +0100

    spec login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { TokenstorageService } from '../services/tokenstorage.service';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +15,35 @@ export class LoginComponent implements OnInit {
   errorMessage='';
   roles: string[] = [];
 
-  constructor() { }
+  constructor(private authservice:AuthService, private tokenservice: TokenstorageService) { }
 
   ngOnInit(): void {
+    if(this.tokenservice.getToken() != null){
+      this.isLoggedIn = true;
+      this.roles = this.tokenservice.getUser().role;
+      alert('on est connecté'+this.isLoggedIn+" role :"+this.roles);
+    }
   }
 
   onSubmit(){
+
+    this.authservice.login(this.form).subscribe(
+
+      data => {
+        this.tokenservice.saveToken(data.token)
+        this.tokenservice.saveUser(JSON.stringify(data));
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenservice.getUser().role;
+        window.location.reload();
+      },
+      error => {
+        this.isLoginFailed = true;
+        this.isLoggedIn = false;
+      }
+
+    );
 
   }
 
