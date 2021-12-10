6381c83901440fab1fb41697de0a3000bf72991d
Mahran-Farhat <farhatmahran@gmail.com>
2021-12-10 14:12:24 +0100

    final version

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenstorageService } from '../services/tokenstorage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,19 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  form:any ={};
-  isLoggedIn = false;
-  isLoginFailed = false;
-  errorMessage='';
-  roles: string[] = [];
+  private roles:string[]=[];
 
-  constructor() { }
+  isLoggedIn = false;
+  showAdminpart = false;
+  ShowUserpart = false;
+  showSecpart = false;
+  username!: string;
+
+  constructor(private tokenservice: TokenstorageService) { }
 
   ngOnInit(): void {
+
+    this.isLoggedIn = !!this.tokenservice.getToken();
+
+    if(this.isLoggedIn){
+      const user = this.tokenservice.getUser();
+      this.roles = user.role;
+
+      this.showAdminpart = this.roles.includes('ROLE_ADMIN');
+      this.ShowUserpart = this.roles.includes('ROLE_USER');
+      this.showSecpart = this.roles.includes('ROLE_SECRETAIRE');
+
+      this.username = user.username;
+
+    }
+
   }
 
   logout(){
-
+    this.tokenservice.signOut();
+    window.location.reload();
   }
 
 }
